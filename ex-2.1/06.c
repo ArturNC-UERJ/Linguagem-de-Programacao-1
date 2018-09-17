636d92083df0b2888b468fae7078897fcc96c551
Artur <arturnascimento11@hotmail.com>
2018-09-16 22:54:48 -0300

    Create 06.c

---
+++ ex-2.1/06.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+//Calcular o somatório desejado
+
+int main()
+{
+    int N, somatorio = 0, i;
+    printf("Insira o limite do somatorio: ");
+    scanf( "%d" , &N );
+    for ( i = 1 ; i <= N ; i++ )
+        {
+            somatorio = somatorio + pow(i , 2);
+        }
+    printf("\nSomatorio realizado: %d\n", somatorio);
+    return 0;
+}
