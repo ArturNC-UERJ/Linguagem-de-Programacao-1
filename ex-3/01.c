aa5a123cee4322a837328265c9e1f7de762997e7
Artur <arturnascimento11@hotmail.com>
2018-10-01 22:01:00 -0300

    Update 01.c

--- ex-3/01.c
+++ ex-3/01.c
@@ -6,9 +6,9 @@ int main(void)
 {
     int i, qtd;
     float soma, media, temporario;
-    char temps[5];
+    char temps[50];
     i = qtd = soma = media = 0;
-    for (i; i < 5; i++)
+    for (i; i < 50; i++)
     {
         printf("Insira uma temperatura (entre -100 e 100): ");
         scanf("%f", &temporario);
@@ -23,9 +23,9 @@ int main(void)
             soma += temps[i];
         }
     }
-    media = soma / 5;
+    media = soma / 50;
     printf("\nA media das temperaturas foi de %.2f\n", media);
-    for (i = 0; i < 5; i++)
+    for (i = 0; i < 50; i++)
     {
         if (temps[i] > media)
         {
