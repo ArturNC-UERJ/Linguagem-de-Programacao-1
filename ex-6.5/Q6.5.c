53104ae08ac14b83a571ab028d88ca24dbad274b
Artur <arturnascimento11@hotmail.com>
2018-10-25 00:05:01 -0300

    Update Q6.5.c

--- ex-6.5/Q6.5.c
+++ ex-6.5/Q6.5.c
@@ -4,12 +4,9 @@ int minha_strlen(char* str){
     int i = 0, tamanho = 0;
     while (str[i] != '\0'){
         tamanho++;
-        i++;
-    }
-    return tamanho;
-}
-void minha_strjoin(char* str3, char* str1, char* str2)
-{
+        i++;}
+    return tamanho;}
+void minha_strjoin(char* str3, char* str1, char* str2){
     int i, j;
     j = minha_strlen(str1);
     for (i = 0; i < strlen(str1); i++){
@@ -17,9 +14,8 @@ void minha_strjoin(char* str3, char* str1, char* str2)
     for (i = 0; i < strlen(str2); i++){
         str3[j] = str2[i];
         j++;}
-    str3[j] = '\0';} //Identificar de final de string.
-int main(void)
-{
+    str3[j] = '\0';}
+int main(void){
     char str1[40], str2[40], str3[100];
     printf("Insira a string 1: ");
     gets(str1);
@@ -27,5 +23,4 @@ int main(void)
     gets(str2);
     minha_strjoin(str3, str1, str2);
     printf("Nova string: %s", str3);
-    return 0;
-}
+    return 0;}
